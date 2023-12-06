46c9b3379e3e50813f45c3163b2a031246ef8855
sN00bFloat <valent.petroff2011@yandex.ru>
2023-12-06 20:49:44 +0300

    Фикс прав репозитория

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/sN00b1/yp-url-shortener/internal/server"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", server.ShortenerHandler)
+	err := http.ListenAndServe("localhost:8080", mux)
+	if err != nil {
+		fmt.Println("ListenAndServe error: ", err)
+	}
+}
