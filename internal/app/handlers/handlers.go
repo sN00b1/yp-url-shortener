2d636b2a85a78446696e20a6543a04a6c72ac930
sN00b1 <117156586+sN00b1@users.noreply.github.com>
2024-01-08 22:03:08 +0300

    Добавлены изменения для прохождения инкремента 7

--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -8,6 +10,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/sN00b1/yp-url-shortener/internal/app/loggin"
 )
 
 type Handler struct {
@@ -68,12 +71,60 @@ func (handler *Handler) Expand(writer http.ResponseWriter, request *http.Request
 	http.Redirect(writer, request, url, http.StatusTemporaryRedirect)
 }
 
+func (handler *Handler) ShortenFromJSON(writer http.ResponseWriter, request *http.Request) {
+	type inputStruct struct {
+		OriginalURL string `json:"original_url"`
+	}
+	var input inputStruct
+	type outputStruct struct {
+		Result string `json:"result"`
+	}
+	var output outputStruct
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(request.Body)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+	}
+	if err = json.Unmarshal(buf.Bytes(), &input); err != nil {
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+	}
+
+	hash, err := handler.generator.MakeHash(string(input.OriginalURL))
+	if hash == "" {
+		http.Error(writer, "cannot generate url", http.StatusInternalServerError)
+		return
+	}
+	handler.storage.Save(string(input.OriginalURL), hash)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	output.Result = fmt.Sprintf("%s/%s", handler.cfg.HandlerURL, hash)
+
+	resp, err := json.Marshal(output)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusCreated)
+	_, err = writer.Write([]byte(resp))
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func NewRouter(handler *Handler) chi.Router {
 	router := chi.NewRouter()
 	router.Use(middleware.Recoverer)
+	router.Use(loggin.LogginResponse)
 	router.Route("/", func(router chi.Router) {
 		router.Get("/{id}", handler.Expand)
 		router.Post("/", handler.Shorten)
+		router.Post("/api/shorten", handler.ShortenFromJSON)
 	})
 	return router
 }
